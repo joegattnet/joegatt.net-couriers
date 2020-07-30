869cd55f4bec14e4107c32841290b3425685c61b
Joe Gatt <joe@joegatt.net>
2020-07-30 23:08:46 +0200

    Add links

--- src/tasks/readGoogleDoc.js
+++ src/tasks/readGoogleDoc.js
@@ -73,12 +73,15 @@ const formatFootnote = (footnoteId, footnotes) => {
   const textArray = footnotes[footnoteId].content.map(chunk => {
     if (!chunk.paragraph) return null;
     return chunk.paragraph.elements.map(element => {
+      if (element.textRun && element.textRun.textStyle.link) {
+        return `<a href="${element.textRun.textStyle.link.url}">${element.textRun.content.trim()}</a>`;
+      }
       if (element.textRun) return element.textRun.content.trim();
       if (element.footnoteReference) return formatFootnote(element.footnoteReference, data.footnotes);      
       return null;
-    })
-  })
-  const textString = textArray.join(' ');
+    }).join('');
+  });
+  const textString = textArray.join('');
   textString.replace(/ +/gm, ' ');
   return `[${textString}]`;
 }
@@ -86,11 +89,16 @@ const formatFootnote = (footnoteId, footnotes) => {
 const formatBody = data => {
   const textArray = data.body.content.map(chunk => {
     if (!chunk.paragraph) return null;
+    if (chunk.paragraph.paragraphStyle.namedStyleType === 'TITLE') return null;
     if (chunk.paragraph.paragraphStyle.namedStyleType === 'HEADING_5') {
       const headingText = chunk.paragraph.elements[0].textRun.content.trim();
+      if (headingText === '--30--' || headingText === '-30-') return headingText;
       return `\{\{${headingText}\}\}`.replace(/\{\{\{\{/, '{{').replace(/\}\}\}\}/, '}}');
     }
     return chunk.paragraph.elements.map(element => {
+      if (element.textRun && element.textRun.textStyle.link) {
+        return `<a href="${element.textRun.textStyle.link.url}">${element.textRun.content.trim()}</a>`;
+      }
       if (element.textRun) return element.textRun.content.trim();
       if (element.footnoteReference) return formatFootnote(element.footnoteReference.footnoteId, data.footnotes);      
       return null;
@@ -98,22 +106,45 @@ const formatBody = data => {
   });
   const textString = textArray.join('\n\n');
   textString.replace(/\n\n\n+/gm, '\n\n').replace(/ +/gm, ' ');
-  return textString.replace(/\n\n\n\n\{\{/gm, '\n\n\n{{').replace(/\}\}\n\n/gm, '}}\n');
+  return textString.replace(/\n\n\n\n\{\{/gm, '\n\n\n{{').replace(/\}\}\n\n/gm, '}}\n').trim();
 }
+
+const chapters = [
+  {
+    name: 'Sample',
+    googleDocumentId: '1mhAQIzBflcx_jxIejYOrfWWgEnjCD6Kpa9eTxFTFdF0'
+  },
+  {}, {}, {},
+  {
+    name: 'Chapter 4',
+    googleDocumentId: '13LhjdMQiQvQJfqy9EhcIICp5Zmp06hnjqRoDCC1shNI'
+  },
+  {
+    name: 'Chapter 5',
+    googleDocumentId: '1BsLoH3GnAWUMss04IcTxPQZkbv0suV3zuYg5r7ZHXgY'
+  }
+];
 
 /**
  * Prints the title of a sample doc:
  * https://docs.google.com/document/d/195j9eDD3ccgjQRttHhJPymLJUCOUjs-jmwTrekvdjFE/edit
  * @param {google.auth.OAuth2} auth The authenticated Google OAuth 2.0 client.
  */
+
 function printDocTitle(auth) {
   const docs = google.docs({version: 'v1', auth});
   docs.documents.get({
-    documentId: '1BsLoH3GnAWUMss04IcTxPQZkbv0suV3zuYg5r7ZHXgY',
+    documentId: chapters[4].googleDocumentId,
   }, (err, res) => {
     if (err) return console.log('The API returned an error: ' + err);
     console.log(`The title of the document is: ${res.data.title}`);
     const bodyText = formatBody(res.data);
+
+    fs.writeFile('content.json', JSON.stringify(res.data), (err) => {
+      if (err) return console.error(err);
+      console.log('Content stored to content.json');
+    });
+
     fs.writeFile('content.txt', bodyText, (err) => {
         if (err) return console.error(err);
         console.log('Content stored to content.txt');
